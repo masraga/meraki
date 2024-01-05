d095dc8f2b199382deea54b98faf72d781471acc
masraga <github.ragamulia@gmail.com>
2024-01-06 01:16:27 +0700

    docs: add rules for creating API

--- routes/api.go
+++ routes/api.go
@@ -7,21 +7,27 @@ import (
 )
 
 func Api(router *gin.Engine) {
-
+	/*
+		define all controller here
+	*/
 	userController := controllers.NewUser()
 	welcomeController := controllers.NewWelcome()
 
+	/*
+		add public api with `api` keyword
+	*/
 	api := router.Group("/api")
-
 	api.GET("/users/hello-world", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello world",
 		})
 	})
-
 	api.POST("/users/register", userController.Register)
 	api.POST("/users/login", userController.Login)
 
+	/*
+		add private api with `apiAuth` keyword
+	*/
 	apiAuth := api.Use(middlewares.VerifyUserToken)
 	apiAuth.GET("/admin/dashboard", welcomeController.Index)
 }
